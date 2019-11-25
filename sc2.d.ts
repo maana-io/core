513059c7b942a28a3e3e43dbcf55f577c97af313
witt3rd <witt3rd@witt3rd.com>
2019-11-25 03:17:31 -0800

    add ts definition for listMaps

--- sc2.d.ts
+++ sc2.d.ts
@@ -2,6 +2,10 @@
 
 export function createAgent(blueprint?: EventReader<AgentObject>): Agent;
 export function createEngine(opts?: EngineOptions): Engine;
-export function createSystem(sys: EventReader<SystemObject>, opts?: SystemOptions): EventReader<System>;
+export function createSystem(
+  sys: EventReader<SystemObject>,
+  opts?: SystemOptions
+): EventReader<System>;
 export function createPlayer(settings, agent?: Agent): any;
 export const taskFunctions: { [index: string]: BuildHelper };
+export function listMaps(): Promise<string[]>;
